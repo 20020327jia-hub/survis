5541e66eec5855aedc5078998e8a9060481f6575
20020327jia-hub <20020327jia@gmail.com>
2026-05-10 03:54:33 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,106 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "benford2022sensitive": {
+        "author": "Benford, Steve and L{\\o}vlie, Anders Sundnes and Ryding, Karin and Rajkowska, Paulina and Bodiaj, Edgar and Darzentas, Dimitrios Paris and Cameron, Harriet R and Spence, Jocelyn and Egede, Joy and Spanjevic, Bogdan",
+        "keywords": "Emotional and Social",
+        "title": "Sensitive Pictures",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "year": "2022"
+    },
+    "eklund2020shoe": {
+        "author": "Eklund, Lina",
+        "journal": "International Journal of Human--Computer Interaction",
+        "keywords": "Emotional and Social",
+        "number": "16",
+        "pages": "1503--1513",
+        "publisher": "Taylor \\& Francis",
+        "title": "A shoe is a shoe is a shoe: Interpersonalization and meaning-making in museums--research findings and design implications",
+        "type": "article",
+        "volume": "36",
+        "year": "2020"
+    },
+    "kim2025multidimensional": {
+        "author": "Kim, Jihyun and Kim, So Yeon",
+        "journal": "Curator: The Museum Journal",
+        "keywords": "Visitor and Data",
+        "number": "4",
+        "pages": "622--636",
+        "publisher": "Wiley Online Library",
+        "title": "Multidimensional analysis of visitor interaction with museum apps: Designing for enhanced museum experiences",
+        "type": "article",
+        "volume": "68",
+        "year": "2025"
+    },
+    "kobeisse2022can": {
+        "author": "Kobeisse, Suzanne and Holmquist, Lars Erik",
+        "booktitle": "Proceedings of the 21st International Conference on Mobile and Ubiquitous Multimedia",
+        "keywords": "Immersive Tech",
+        "pages": "28--36",
+        "title": "\u201cI Can Feel It in My Hand\u201d: Exploring Design Opportunities for Tangible Interfaces to Manipulate Artefacts in AR",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "peppler2021cultivating": {
+        "author": "Peppler, Kylie and Keune, Anna and Han, Ariel",
+        "journal": "Information and Learning Sciences",
+        "keywords": "Visitor and Data",
+        "number": "1-2",
+        "pages": "1--16",
+        "publisher": "Emerald Publishing Limited",
+        "title": "Cultivating data visualization literacy in museums",
+        "type": "article",
+        "volume": "122",
+        "year": "2021"
+    },
+    "ryding2020silent": {
+        "author": "Ryding, Karin",
+        "booktitle": "Proceedings of the 2020 CHI Conference on Human Factors in Computing Systems",
+        "keywords": "Emotional and Social",
+        "pages": "1--10",
+        "title": "The Silent Conversation: Designing for Introspection and Social Play in Art Museums",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "ryding2021interpersonalizing": {
+        "author": "Ryding, Karin and Spence, Jocelyn and L{\\o}vlie, Anders Sundnes and Benford, Steve",
+        "journal": "International Journal of Human--Computer Interaction",
+        "keywords": "Emotional and Social",
+        "number": "12",
+        "pages": "1151--1172",
+        "publisher": "Taylor \\& Francis",
+        "title": "Interpersonalizing intimate museum experiences",
+        "type": "article",
+        "volume": "37",
+        "year": "2021"
+    },
+    "shi2024augmented": {
+        "author": "Shi, Yunzhen and Ghafar, Maszura Abdul and Yahaya, Mohd Faiz",
+        "journal": "Idealogy Journal",
+        "keywords": "Immersive Tech",
+        "number": "2",
+        "title": "Augmented Reality for interactive experiences in museums: A review",
+        "type": "article",
+        "volume": "9",
+        "year": "2024"
+    },
+    "spence2020vrtefacts": {
+        "author": "Spence, Jocelyn and Darzentas, Dimitrios Paris and Huang, Yitong and Cameron, Harriet R and Beestin, Eleanor and Benford, Steve",
+        "booktitle": "Proceedings of the 2020 ACM Designing Interactive Systems Conference",
+        "keywords": "Immersive Tech",
+        "pages": "627--640",
+        "title": "VRtefacts: Performative substitutional reality with museum objects",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "su2023understanding": {
+        "author": "Su, Man and Ha, Jesse and Cort{\\'e}s, Luis E P{\\'e}rez and Bernier, Jeremy and Yan, Lin and Nelson, Brian C and Bowman, Judd D and Bowman, Catherine DD",
+        "journal": "Educational technology research and development",
+        "keywords": "Visitor and Data",
+        "number": "6",
+        "pages": "2483--2506",
+        "publisher": "Springer",
+        "title": "Understanding museum visitors\u2019 question-asking through a mobile app",
+        "type": "article",
+        "volume": "71",
+        "year": "2023"
     }
 };
